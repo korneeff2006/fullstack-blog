7e8efbbcec50ba3e60fd6412c0af081ee43f0efd
Korneeff <korneeff2006@yandex.ru>
2019-03-27 21:20:01 +0300

    added mongoose and express

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const keys =require('./keys')
+
+const app = express()
+const clientpath = path.join(__dirname , 'client')
+
+mongoose.connect(keys.mongoURI ,  { useNewUrlParser: true })
+    .then( () => {
+        console.log('MongoDB connected')
+    })
+
+    .catch((err) => {
+        console.error(err)
+    })
+
+
+app.use(express.static(clientpath))
+
+
+const port = process.env.port || 5000
+
+app.listen(port , () => {
+    console.log('Server has been started on port: ' + port + '!')
+})
---
+++ keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb://mongo-admin:daydream@localhost:27017/fullstack-blog'  
+}
+
+
+
+
